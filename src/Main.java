cf4e2219da8b9da88683c5434949125d33a97353
Joon P <Park2@iastate.edu>
2023-04-15 18:00:51 -0500

    Syllabus.class to Course

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Moe");
 
+        System.out.println(Calender.CurrentDateTime());
     }
-
 }
