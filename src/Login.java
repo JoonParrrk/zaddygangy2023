cd6f18123e7df6e8bbf73b7f1e73bcf4f6e19d3c
Kalem S <kalemschrock@gmail.com>
2023-04-15 16:54:00 -0500

    login info file

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,27 @@
 public class Login {
+
+    private String username;
+
+    private String password;
+
+    private int accountNumber;
+
+    public Login(String username, String password, int accountNumber) {
+        this.username = username;
+        this.password = password;
+        this.accountNumber = accountNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
     
 }
