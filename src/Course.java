139c71e0d8ebe96b35f762a3ffebbbed0a317832
Joon P <Park2@iastate.edu>
2023-04-15 18:08:32 -0500

    Error fixed

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Course {
@@ -6,53 +7,11 @@ public class Course {
     private HashSet<Task> exams;
     private HashSet<Task> quizzes;
     private HashSet<Task> assignments;
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public ArrayList<Task> getQuizzes() {
-        return quizzes;
-    }
-
-    public void setQuizzes(ArrayList<Task> quizzes) {
-        this.quizzes = quizzes;
-    }
-
-    public ArrayList<Task> getExams() {
-        return exams;
-    }
-
-    public void setExams(ArrayList<Task> exams) {
-        this.exams = exams;
-    }
-
-    public ArrayList<Task> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(ArrayList<Task> projects) {
-        this.projects = projects;
-    }
-
-    public ArrayList<Task> getAssignments() {
-        return assignments;
-    }
-
-    public void setAssignments(ArrayList<Task> assignments) {
-        this.assignments = assignments;
-    }
-
-    private ArrayList<Task> exams;
-    private ArrayList<Task> projects;
-    private ArrayList<Task> assignments;
+    private HashMap<String, Double> weight;
 
     Course(String name) {
         courseName = name;
+
 
     }
 
