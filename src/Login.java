97c059f60a974574b0ff3dcbc69cd4cd59473f42
Kalem S <kalemschrock@gmail.com>
2023-04-15 16:55:10 -0500

    added getters and setters to Login

--- src/Login.java
+++ src/Login.java
@@ -12,6 +12,14 @@ public class Login {
         this.accountNumber = accountNumber;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
