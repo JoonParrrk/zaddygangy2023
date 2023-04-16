55a55d5a2c3bba2cadd0a1f626a6c9dc85c137be
Moe <bigboimo@iastate.edu>
2023-04-15 23:33:44 -0500

    Deleted taskList

--- src/User.java
+++ src/User.java
@@ -1,20 +1,2 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class User {
-
-    private List<StudySession> StudyTimes;
-
-    public User() {
-        StudyTimes = new ArrayList<>();
-    }
-    
-    public void addStudySession(StudySession session) {
-        StudyTimes.add(session);
-    }
-
-    public List<StudySession> getStudyTime() {
-        return StudyTimes;
-    }
-
 }
