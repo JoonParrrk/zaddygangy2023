ffba80da858046e1d46329b9b9f5b28722abbdb4
ManjulBalayar <mbalayar@iastate.edu>
2023-04-15 23:27:14 -0500

    AnothaDaddy

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Moe");
         System.out.println("JOON");
+        
+        for(int i = 0; i < 9; i++) {
+            System.out.println(i);
+        }
 
     }
 }
