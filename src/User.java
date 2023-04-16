e8974947eb8c5dc40acf42cf56dfa7832f376c9a
ManjulBalayar <mbalayar@iastate.edu>
2023-04-15 23:32:07 -0500

    AnothaAnotherDaddy

--- src/User.java
+++ src/User.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private List<StudySession> StudyTimes;
+
+    public User() {
+        StudyTimes = new ArrayList<>();
+    }
+    
+    public void addStudySession(StudySession session) {
+        StudyTimes.add(session);
+    }
+
+    public List<StudySession> getStudyTime() {
+        return StudyTimes;
+    }
+
 }
