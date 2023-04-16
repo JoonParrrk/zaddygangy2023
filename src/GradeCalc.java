54ae77132d3993e013d3793b38b1ff1daffbed5e
Joon P <Park2@iastate.edu>
2023-04-15 23:12:38 -0500

    Attempt #1

--- src/GradeCalc.java
+++ src/GradeCalc.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class GradeCalc {
     private double goalGrade;
     private double currentGrade;
     private double projectedGrade;
+
+    public static double getCurrentGrades(HashSet<Task> exams, HashSet<Task> quizzes, HashSet<Task> assignments,
+                                          HashSet<Task> projects, HashMap<String, Double> weight, double extraCredit) {
+
+        // do calculations
+
+        return 0;
+    }
 
 }
